799dda127c9a25cfc5480e9a00f32d54bf35d9a5
Thegodfather510 <moad.wins5@gmail.com>
2021-10-16 14:15:27 +0100

    Quizz Test
    Quizz Test based on different questions and answers with the given score

---
+++ js/questions.js
@@ -0,0 +1,106 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Que signifie HTML?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+
+    {
+    numb: 2,
+    question: "Que signifie CSS?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Que signifie PHP",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Que signifie SQL?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Que signifie XML",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+    {
+    numb: 6,
+    question: "Si nous voulons définir le style d'un élément unique, quel sélecteur css utiliserons-nous?",
+    answer: "ID",
+    options: [
+      "Class",
+      "text",
+      "name",
+      "ID"
+     ]
+   },
+
+   {
+    numb: 7,
+    question: "Si nous ne voulons pas autoriser un div flottant sur le côté gauche d'un élément, quelle propriété css utiliserons-nous?",
+    answer: "clear",
+    options: [
+      "margin",
+      "padding",
+      "clear",
+      "float"
+     ]
+   }, 
+
+   {
+    numb: 9,
+    question: "Si nous voulons utiliser une belle bordure en pointillé vert autour d'une image, quelle propriété css utiliserons-nous?",
+    answer: "border-style",
+    options: [
+      "border-style",
+      "border-decoration",
+      "border-color",
+      "border-line"
+     ]
+   }, 
+
+   {
+    numb: 10,
+    question: "Si nous voulons envelopper un bloc de texte autour d'une image, quelle propriété css utiliserons-nous?",
+    answer: "float",
+    options: [
+      "wrap",
+      "push",
+      "float",
+      "allign"
+     ]
+   }, 
+];
